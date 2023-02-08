44078763f23e47804559257ad4c1014cbfd65009
Icarus42 <fabian.penezic@fer.hr>
2023-02-08 22:55:39 +0100

    Refactor part I

--- Src/UI.cpp
+++ Src/UI.cpp
@@ -18,7 +18,6 @@
 #include "taskManagement.h"
 #endif
 
-
 //Should be part of taskManagement
 template <typename F>
 void forEachNodeDo(std::shared_ptr<Task> curTask, F&& func, int d){
@@ -27,6 +26,8 @@ void forEachNodeDo(std::shared_ptr<Task> curTask, F&& func, int d){
                 forEachNodeDo(*it, func, d+1);
         }
 }
+
+//########################################[COMP_DOMAIN METHODS]##############################
 
 comp_domain::comp_domain(int x_from, int x_to, int y_from, int y_to){
         x.first = x_from;
@@ -53,12 +54,26 @@ UI_Comp<T>::UI_Comp(){
         obj = nullptr;
 }
 
+//########################################[EXTERNAL DATA]##############################
 extern int focused, currentProjs, curentProjIndx;
 extern std::vector<std::string> projectNames;
 extern std::vector<std::string> projectFileNames;
 extern std::vector<std::shared_ptr<Task>> taskRoots;
+
+//########################################[INTERNAL DATA]##############################
 std::vector<UI_Comp<Task>> taskComps;
 static size_t currUiCompIndx = 0;
+
+//########################################[STATIC FUNCTION PROTOTYPES]##############################
+void drawTaskComp(UI_Comp<Task> comp);
+void createUiComps();
+void loadTodo(std::string projecFileName);
+void displayTodo();
+void renameTask(UI_Comp<Task> &comp);
+void createNewTask(UI_Comp<Task> &comp, bool newRoot);
+void deleteTask(UI_Comp<Task> &comp);
+
+//########################################[INTERFACE FUNCTION DEFINITIONS]##############################
 
 void initCurses(){
         initscr();
@@ -68,6 +83,7 @@ void initCurses(){
         clear();
 }
 
+//Should not be here
 int loadProject(char *fileName){
         int retVal = 0;
         size_t n = 0;
@@ -124,162 +140,6 @@ void highlightProj(int x_from, int x_to, int y_from, int y_to, int color, int mo
         }
         move(before_y, before_x);
         refresh();
-}
-
-void drawTaskComp(UI_Comp<Task> comp){
-        //Save the current cursor position
-        int cury, curx;
-        getyx(stdscr, cury, curx);
-        //Draw the task
-        move(comp.domain.y.first, comp.domain.x.first);
-        addstr(comp.obj->desc.c_str());
-        addstr(" ");
-        if (comp.obj->Completed)
-                addstr("T");
-        else
-                addstr("F");
-        //restore cursor position
-        move(cury, curx);
-}
-
-void createUiComps(){
-        int i = 0;
-        taskComps.clear();
-        //Transverses every tree and creates a UI_Component for every tree node(Task)
-        for (auto it = taskRoots.begin(); it != taskRoots.end(); it++){
-                std::vector<std::pair<std::shared_ptr<Task>,int>> stack;
-                stack.push_back(std::pair<std::shared_ptr<Task>,int>(*it, 0));
-                while (!stack.empty()){
-                        std::shared_ptr<Task> cur = stack.back().first;
-                        int d = stack.back().second;
-                        stack.pop_back();
-                        struct comp_domain domain(LEFT_RIGHT_BORDER + 5 + d *4, COLS-5, i, i+2);
-                        i+=2;
-                        taskComps.push_back(UI_Comp<Task>(domain, cur));
-                        for (auto it = cur->subTasks.begin(); it != cur->subTasks.end();it++){
-                                stack.push_back(std::pair<std::shared_ptr<Task>,int>(*it, d+1));
-                        }
-                }
-        }
-}
-
-void loadTodo(std::string projecFileName){
-        taskRoots.clear();
-        taskRoots = parseFile(projecFileName);
-        createUiComps();
-}
-
-void displayTodo(){
-        for (auto it = taskComps.begin(); it != taskComps.end(); it++){
-                drawTaskComp(*it);
-        }
-        refresh();
-}
-
-void renameTask(UI_Comp<Task> &comp){
-        std::stringstream s;
-        //save current pos
-        int cury, curx; 
-        getyx(stdscr, cury, curx);
-        //move to beginning of the text
-        move(comp.domain.y.first, comp.domain.x.first);
-        curs_set(2);
-        clrtoeol();
-        move(comp.domain.y.first, comp.domain.x.first);
-        int input_ch;
-        bool running = true;
-        while (running){
-                input_ch = getch();
-                switch (input_ch) {
-                        //Accept the rename, ENTER
-                        case 10:
-                                comp.obj->desc = s.str();
-                                running = false;
-                                break;
-                        //Discard the rename, ESC
-                        case 27:
-                                running = false;
-                                break;
-                        case KEY_BACKSPACE:
-                                int cury, curx;
-                                getyx(stdscr, cury, curx);
-                                move(cury, curx-1);
-                                clrtoeol();
-                                s.seekp(-1, s.cur);
-                                break;
-                        default:
-                                if ( (input_ch >= 'a' && input_ch <= 'z') || 
-                                                (input_ch >= 'A' && input_ch <= 'Z') ||
-                                                (input_ch >= '0' && input_ch <= '9') ||
-                                                input_ch == ' '
-                                                ){
-                                        s << (char)input_ch;
-                                        addch(input_ch);
-                                }
-                                break;
-                }
-        }
-        move(comp.domain.y.first, comp.domain.x.first);
-        addstr(comp.obj->desc.c_str());
-        addch(' ');
-        addch(comp.obj->Completed? 'T':'F');
-        curs_set(0);
-        move(cury, curx);
-        refresh();
-}
-
-void createNewTask(UI_Comp<Task> &comp, bool newRoot){
-        if (newRoot){
-                taskRoots.push_back(std::shared_ptr<Task>(new Task("", false)));
-        }
-        else{
-                comp.obj->subTasks.push_back(std::shared_ptr<Task>(new Task("", false))); 
-        }
-        createUiComps();
-        clear();
-        move(0, 0);
-        displayProjects();
-        move(0, LEFT_RIGHT_BORDER);
-        vline(ACS_VLINE, LINES);
-        displayTodo();
-        for (auto  it = taskComps.begin(); it != taskComps.end(); it++)
-                if (it->obj->desc == ""){
-                        renameTask(*it);
-                        break;
-                }
-}
-
-void deleteTask(UI_Comp<Task> &comp){
-        auto func = [&,comp](std::shared_ptr<Task> curTask, int d){
-                for (auto subTask = curTask->subTasks.begin(); subTask != curTask->subTasks.end();
-                                subTask++)
-                        if ((*subTask)->desc == comp.obj->desc){
-                                curTask->subTasks.erase(subTask);
-                                break;
-                        }
-        };
-        for (auto it = taskRoots.begin(); it != taskRoots.end(); it++){
-                if ( (*it)->desc ==comp.obj->desc){
-                        taskRoots.erase(it);
-                        break;
-                }
-                else{
-                        forEachNodeDo(*it, func, 0);
-                }
-        }
-        for (auto it = taskComps.begin(); it != taskComps.end(); it++){
-                if ( it->obj == comp.obj){
-                        taskComps.erase(it);
-                        break;
-                }
-        }
-        createUiComps();
-        clear();
-        move(0, 0);
-        displayProjects();
-        move(0, LEFT_RIGHT_BORDER);
-        vline(ACS_VLINE, LINES);
-        displayTodo();
 }
 
 int parseCommandRight(int input_ch){
@@ -420,6 +280,8 @@ int parseCommandLeft(int input_ch){
                         highlightProj(0, LEFT_RIGHT_BORDER - 1, cury, cury, COLOR_WHITE, A_NORMAL);
                         cury = 0;
                         break;
+		case 'n'://create a new project
+			
                 default:
                         break;
         }
@@ -433,3 +295,163 @@ int parseCommandLeft(int input_ch){
         refresh();
         return running;
 }
+
+//########################################[STATIC FUNCTION DEFINITIONS]##############################
+
+void drawTaskComp(UI_Comp<Task> comp){
+        //Save the current cursor position
+        int cury, curx;
+        getyx(stdscr, cury, curx);
+        //Draw the task
+        move(comp.domain.y.first, comp.domain.x.first);
+        addstr(comp.obj->desc.c_str());
+        addstr(" ");
+        if (comp.obj->Completed)
+                addstr("T");
+        else
+                addstr("F");
+        //restore cursor position
+        move(cury, curx);
+}
+
+void createUiComps(){
+        int i = 0;
+        taskComps.clear();
+        //Transverses every tree and creates a UI_Component for every tree node(Task)
+        for (auto it = taskRoots.begin(); it != taskRoots.end(); it++){
+                std::vector<std::pair<std::shared_ptr<Task>,int>> stack;
+                stack.push_back(std::pair<std::shared_ptr<Task>,int>(*it, 0));
+                while (!stack.empty()){
+                        std::shared_ptr<Task> cur = stack.back().first;
+                        int d = stack.back().second;
+                        stack.pop_back();
+                        struct comp_domain domain(LEFT_RIGHT_BORDER + 5 + d *4, COLS-5, i, i+2);
+                        i+=2;
+                        taskComps.push_back(UI_Comp<Task>(domain, cur));
+                        for (auto it = cur->subTasks.begin(); it != cur->subTasks.end();it++){
+                                stack.push_back(std::pair<std::shared_ptr<Task>,int>(*it, d+1));
+                        }
+                }
+        }
+}
+
+void loadTodo(std::string projecFileName){
+        taskRoots.clear();
+        taskRoots = parseFile(projecFileName);
+        createUiComps();
+}
+
+void displayTodo(){
+        for (auto it = taskComps.begin(); it != taskComps.end(); it++){
+                drawTaskComp(*it);
+        }
+        refresh();
+}
+
+void renameTask(UI_Comp<Task> &comp){
+        std::stringstream s;
+        //save current pos
+        int cury, curx; 
+        getyx(stdscr, cury, curx);
+        //move to beginning of the text
+        move(comp.domain.y.first, comp.domain.x.first);
+        curs_set(2);
+        clrtoeol();
+        move(comp.domain.y.first, comp.domain.x.first);
+        int input_ch;
+	//should all be a function for itself
+        bool running = true;
+        while (running){
+                input_ch = getch();
+                switch (input_ch) {
+                        //Accept the rename, ENTER
+                        case 10:
+                                comp.obj->desc = s.str();
+                                running = false;
+                                break;
+                        //Discard the rename, ESC
+                        case 27:
+                                running = false;
+                                break;
+                        case KEY_BACKSPACE:
+                                int cury, curx;
+                                getyx(stdscr, cury, curx);
+                                move(cury, curx-1);
+                                clrtoeol();
+                                s.seekp(-1, s.cur);
+                                break;
+                        default:
+                                if ( (input_ch >= 'a' && input_ch <= 'z') || 
+                                                (input_ch >= 'A' && input_ch <= 'Z') ||
+                                                (input_ch >= '0' && input_ch <= '9') ||
+                                                input_ch == ' '
+                                                ){
+                                        s << (char)input_ch;
+                                        addch(input_ch);
+                                }
+                                break;
+                }
+        }
+        move(comp.domain.y.first, comp.domain.x.first);
+        addstr(comp.obj->desc.c_str());
+        addch(' ');
+        addch(comp.obj->Completed? 'T':'F');
+        curs_set(0);
+        move(cury, curx);
+        refresh();
+}
+
+void createNewTask(UI_Comp<Task> &comp, bool newRoot){
+        if (newRoot){
+                taskRoots.push_back(std::shared_ptr<Task>(new Task("", false)));
+        }
+        else{
+                comp.obj->subTasks.push_back(std::shared_ptr<Task>(new Task("", false))); 
+        }
+        createUiComps();
+        clear();
+        move(0, 0);
+        displayProjects();
+        move(0, LEFT_RIGHT_BORDER);
+        vline(ACS_VLINE, LINES);
+        displayTodo();
+        for (auto  it = taskComps.begin(); it != taskComps.end(); it++)
+                if (it->obj->desc == ""){
+                        renameTask(*it);
+                        break;
+                }
+}
+
+void deleteTask(UI_Comp<Task> &comp){
+        auto func = [&,comp](std::shared_ptr<Task> curTask, int d){
+                for (auto subTask = curTask->subTasks.begin(); subTask != curTask->subTasks.end();
+                                subTask++)
+                        if ((*subTask)->desc == comp.obj->desc){
+                                curTask->subTasks.erase(subTask);
+                                break;
+                        }
+        };
+        for (auto it = taskRoots.begin(); it != taskRoots.end(); it++){
+                if ( (*it)->desc ==comp.obj->desc){
+                        taskRoots.erase(it);
+                        break;
+                }
+                else{
+                        forEachNodeDo(*it, func, 0);
+                }
+        }
+        for (auto it = taskComps.begin(); it != taskComps.end(); it++){
+                if ( it->obj == comp.obj){
+                        taskComps.erase(it);
+                        break;
+                }
+        }
+        createUiComps();
+        clear();
+        move(0, 0);
+        displayProjects();
+        move(0, LEFT_RIGHT_BORDER);
+        vline(ACS_VLINE, LINES);
+        displayTodo();
+}
+
